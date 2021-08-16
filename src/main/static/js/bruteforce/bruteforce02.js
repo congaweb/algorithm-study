9f8c723263980e813e342c4b1a5f8c8448d20703
barum_park <barumming@gmail.com>
2021-08-16 10:27:49 +0900

    feat: change the code from java to javascript

---
+++ src/main/static/js/bruteforce/bruteforce02.js
@@ -0,0 +1,30 @@
+function solution(N, M) {
+    let answer = '';
+    let selected = Array.from({length: M + 1}, () => 0);
+
+    function rec_fuc(k) {
+        if (k === M + 1) {
+            for (let i = 1; i <= M; i++) {
+                answer += selected[i] + " ";
+            }
+            answer += "\n";
+        } else {
+            for (let cand = 1; cand <= M; cand++) {
+                let isUsed = false;
+                for (let i = 1; i <= k; i++) {
+                    if (cand === selected[i]) isUsed = true;
+                }
+                if (!isUsed) {
+                    selected[k] = cand;
+                    rec_fuc(k + 1);
+                    selected[k] = 0;
+                }
+            }
+        }
+    }
+
+    rec_fuc(1);
+    return answer;
+}
+
+console.log(solution(3, 3))
