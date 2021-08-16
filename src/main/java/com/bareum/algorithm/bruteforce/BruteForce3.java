6c1f852495812ade0b618bf9c7f0c48ac1028128
barum_park <barumming@gmail.com>
2021-08-16 20:16:59 +0900

    feat: Study BruteForce with java
    Question number is BOJ 15652, 15650

---
+++ src/main/java/com/bareum/algorithm/bruteforce/BruteForce3.java
@@ -0,0 +1,89 @@
+package com.bareum.algorithm.bruteforce;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/* 백준 15652번 N과 M(4)  */
+/* https://www.acmicpc.net/problem/15652 */
+
+public class BruteForce3 {
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+    }
+
+    static int N, M; // 입력값 받을 변수
+    static int[] selected; // 결과 저장하는 배열
+
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        } else {
+            int start = selected[k-1];
+            if(start == 0) start = 1;
+            for (int cand = start; cand <= N; cand++) {
+                selected[k] = cand;
+                rec_func(k+1);
+                selected[k] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
